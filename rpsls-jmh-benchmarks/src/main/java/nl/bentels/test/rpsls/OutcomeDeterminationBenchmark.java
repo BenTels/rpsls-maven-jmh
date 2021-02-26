8c6512389b66680473e7f68daa4e2a51ee63d418
Benjamin Ze'ev Tels <bzt@bentels.nl>
2021-02-27 00:55:06 +0100

    Added performance benchmark as a jmh test

--- rpsls-jmh-benchmarks/src/main/java/nl/bentels/test/rpsls/OutcomeDeterminationBenchmark.java
+++ rpsls-jmh-benchmarks/src/main/java/nl/bentels/test/rpsls/OutcomeDeterminationBenchmark.java
@@ -15,33 +15,6 @@ import java.util.concurrent.TimeUnit;
 
 public class OutcomeDeterminationBenchmark {
 
-    @Test
-    public void launchBenchmark() throws Exception {
-
-        Options opt = new OptionsBuilder()
-                // Specify which benchmarks to run.
-                // You can be more specific if you'd like to run only one benchmark per test.
-                .include(this.getClass().getName() + ".*")
-                // Set the following options as needed
-                .mode (Mode.AverageTime)
-                .timeUnit(TimeUnit.MICROSECONDS)
-                .warmupTime(TimeValue.seconds(1))
-                .warmupIterations(2)
-                .measurementTime(TimeValue.seconds(1))
-                .measurementIterations(2)
-                .threads(2)
-                .forks(1)
-                .shouldFailOnError(true)
-                .shouldDoGC(true)
-                //.jvmArgs("-XX:+UnlockDiagnosticVMOptions", "-XX:+PrintInlining")
-                //.addProfiler(WinPerfAsmProfiler.class)
-                .build();
-
-        new Runner(opt).run();
-    }
-
-
-
     @State(Scope.Thread)
     public static class GameAndEntities {
         public App game = new App();
